590682b915a003ebc32ded22dff7ef3e99026ce2
Andrew Evripidou <aevripidou1996@gmail.com>
2023-02-28 15:31:18 +1100

    added array information

---
+++ WK3-Challenge/script.js
@@ -0,0 +1,35 @@
+var button = document.querySelector ('#generate');
+
+function generatePassword() {
+  var
+}
+
+//Array information//
+var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', "l", 'M', "N", 'O', 'P', "Q", "R", "S", 'T', 'U', 'V', "W", "X", 'Y', 'Z'];
+var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', "n", 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9,];
+var characters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '='];
+
+
+var resultArray = [];
+var userArray = [];
+
+uppercaseLetters [1]
+
+var numChareacter = prompt ('How many numbers & characters would you like in your password?');
+var numbers = confirm ("Would you like numbers in your password?");
+var uppercase = confirm ('Would you like uppercase letters in your password?');
+var lowercase = confirm ("Would you like special characters in your password?");
+
+
+
+
+//rules of the Array
+if (numbers){
+  resultArray = resultArray.concat(numbers);
+
+}
+
+if (uppercase){
+  resultArray = resultArray.concat(uppercaseLetters);
+}
