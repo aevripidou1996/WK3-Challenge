211f57e2e118c130ec2bb62bb7aea95c00ead7b8
Andrew Evripidou <aevripidou1996@gmail.com>
2023-02-28 22:07:01 +1100

    fixed error L25

--- WK3-Challenge/script.js
+++ WK3-Challenge/script.js
@@ -1,59 +1,60 @@
 var button = document.querySelector('#generate');
 
-function generatePassword(){
-  var
+function generatePassword() {
 
 
+  //Array information//
+  var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', "l", 'M', "N", 'O', 'P', "Q", "R", "S", 'T', 'U', 'V', "W", "X", 'Y', 'Z'];
+  var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', "n", 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9,];
+  var characters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '='];
 
 
-//Array information//
-var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', "l", 'M', "N", 'O', 'P', "Q", "R", "S", 'T', 'U', 'V', "W", "X", 'Y', 'Z'];
-var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', "n", 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9,];
-var characters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '='];
+  var resultArray = [];
+  var userArray = [];
+
+  // uppercaseLetters[0]
+
+  var numChareacter = prompt('How many numbers & characters would you like in your password?');
+  var numbers = confirm("Would you like numbers in your password?");
+  var uppercase = confirm('Would you like uppercase letters in your password?');
+  var lowercase = confirm('Would you like lower case letters in your password?');
+  var characters = confirm("Would you like special characters in your password?");
 
 
-var resultArray = [];
-var userArray = [];
-
-uppercaseLetters[1]
-
-var numChareacter = prompt('How many numbers & characters would you like in your password?');
-var numbers = confirm("Would you like numbers in your password?");
-var uppercase = confirm('Would you like uppercase letters in your password?');
-var lowercase = confirm('Would you like lower case letters in your password?');
-var characters = confirm("Would you like special characters in your password?");
+  if (numbers) {
+    resultArray = resultArray.concat(numbers);
+  
+  }
+  
+  if (uppercase) {
+    resultArray = resultArray.concat(uppercaseLetters);
+  }
+  
+  if (lowercase) {
+    resultArray = resultArray.concat(lowercaseLetters);
+  }
+  
+  if (characters) {
+    resultArray = resultArray.concat(characters);
+  }
+  console.log(resultArray);
+  
+  for (var i = 0; i < numChareacter; i++) {
+    userArray.push(resultArray[Math.floor(Math.random() * resultArray.length)]);
+  }
+  
 }
 
 
 //if Statments
-if (numbers) {
-  resultArray = resultArray.concat(numbers);
 
-}
+// return userArray.join("");
 
-if (uppercase) {
-  resultArray = resultArray.concat(uppercaseLetters);
-}
 
-if (lowercase) {
-  resultArray = resultArray.concat(lowercaseLetters);
-}
-
-if (characters) {
-  resultArray = resultArray.concat(characters);
-}
-console.log(resultArray);
-
-for (var i = 0; i < numCharacter; i++) {
-  userArray.push(resultArray[Math.floor(Math.random() * resultArray.length)]);
-}
-
-return userArray.join("");
-}
 
 //create a pasword
-function writePassword(){
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -61,7 +62,7 @@ function writePassword(){
 }
 
 
-function copyToClipboard(){
+function copyToClipboard() {
 }
 
-generatePassword.addEventListener('click', writePassword);
+button.addEventListener('click', writePassword);
